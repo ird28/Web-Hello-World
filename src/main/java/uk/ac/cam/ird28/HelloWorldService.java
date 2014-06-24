64ce805684b1b081cf0dabc483454aaf8362b4a4
Isaac Dunn <ird28@cam.ac.uk>
2014-06-24 13:24:37 +0100

    Added POST ability

--- src/main/java/uk/ac/cam/ird28/HelloWorldService.java
+++ src/main/java/uk/ac/cam/ird28/HelloWorldService.java
@@ -1,14 +1,26 @@
 package uk.ac.cam.ird28;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.core.Response;
 
-@Path("/")
+@Path("/test")
 public class HelloWorldService {
 	
 	@GET
+	@Path("/form")
 	public Response responseMsg() {
-		return Response.status(200).entity("Hello World!").build();
+		String form = "<html><body><form method=\"POST\">Put a word here:<input type=\"text\" name = \"in\"><input type=\"submit\" value=\"Submit\"></form></body></html>";
+		return Response.status(200).entity(form).build();
 	}
+	
+	@POST
+	@Path("/form")
+	public Response reply(@FormParam("in") String word) {
+		return Response.status(200).entity("Your word was " + word).build();
+	}
+	
 }
